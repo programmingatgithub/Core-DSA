b9d05f018c8c619d53d77490f74112e577ba16cb
Mayank Verma <80618915+programmingatgithub@users.noreply.github.com>
2024-10-15 13:09:48 +0530

    Add files via upload

---
+++ binary.cpp
@@ -0,0 +1,24 @@
+// write a program to convert the digit from decimal to binary;
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cout<< "enter the value of n"  <<endl;
+    cin >> n;
+
+    int ans = 0;
+    int i = 0;
+    while ( n != 0){
+        int bit = n & 1;
+        ans = (bit * pow(10,i)) + ans;
+
+        n = n >> 1;
+        i++;
+    }
+    cout << " answer is " << ans << endl;
+
+}
